508e71aa1f398d1d79569c327d77b26dd375b6ac
seulda <kim02977@naver.com>
2021-08-15 02:26:50 +0900

    api error set

--- src/main/java/org/corona/controller/StateController.java
+++ src/main/java/org/corona/controller/StateController.java
@@ -14,7 +14,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.AllArgsConstructor;
@@ -38,21 +37,19 @@ public class StateController {
 
 		String eDay = service.today();	// 기준일 (=종료일)
 		String sDay = service.day(eDay);	// 기준일-10일 (=시작일)
-		String check = service.getCovidStateApi(sDay, eDay);
-		if (check.equals("e")) { // error page
+		
+		String checkCovid = service.getCovidStateApi(sDay, eDay);
+		String checkASA = service.getAreaApi(eDay, eDay);
+		if (checkCovid.equals("e") || checkASA.equals("e")) { // api error
 			model.addAttribute("state", service.Crawler());
+			model.addAttribute("e", "e");
 			return "/layout/info";
 		}
-//		if (check.equals("e")) { // api가 업데이트가 안 된 경우
-//			eDay = service.yday(eDay);
-//			sDay = service.yday(sDay);
-//			check = service.getCovidStateApi(sDay, eDay);
-//		}
-		ArrayList<StateVO> slist = service.covidState(check);
+		
+		ArrayList<StateVO> slist = service.covidState(checkCovid);
 		ArrayList<StateVO> alist = service.aCovidState(slist);
-		log.info("aCovidState alist: " + alist);
 		model.addAttribute("alist", alist);
-		ArrayList<AreaVO> arealist = service.asaArea(service.getAreaApi(eDay, eDay));
+		ArrayList<AreaVO> arealist = service.asaArea(checkASA);
 		model.addAttribute("arealist", arealist);
 		
 		return "/dailyAll/dashboard";
@@ -65,7 +62,6 @@ public class StateController {
 		model.addAttribute("state", service.Crawler());
 		
 		ArrayList<DisasterVO> dlist = service.DisasterMsg(service.getDisasterMsgApi());
-		log.info("DisasterMsg dlist: " + dlist);
 		if(dlist.size()==0) {
 			model.addAttribute("n", "n");
 		} else {
